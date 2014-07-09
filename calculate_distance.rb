a17c2f3f51a74881a532c55f54ce5af299464a90
maximillian2 <maxi.ua.1996@gmail.com>
2014-07-09 14:16:50 +0300

    Small fix

--- calculate_distance.rb
+++ calculate_distance.rb
@@ -1,8 +1,8 @@
 File.open(ARGV[0]).each_line do |line|
-  array = line.scan(/\d+/)
-  puts array.inspect
+  array = line.scan(/-?\d+/)
   x1, y1, x2, y2 = array[0].to_i, array[1].to_i, array[2].to_i, array[3].to_i
 
-  first, second = (x1-x2)**2, (y1-y2)**2
-  puts Math.sqrt(first + second)
+  first, second = (x2-x1)**2, (y2-y1)**2
+
+  puts Math.sqrt(first + second).to_i
 end
